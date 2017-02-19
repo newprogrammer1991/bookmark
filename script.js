7ad3046714bf82a11a3cda3fe1ca42107a3fa15f
Ali <ali.kholmatov@gmail.com>
2017-02-20 00:42:02 +0200

    update

--- script.js
+++ script.js
@@ -1,107 +1,115 @@
 
+"use strict";
 
- document.getElementsByName('siteName')[0].focus();
+document.getElementsByName('siteName')[0].focus();
 window.onload=init;
 let storedSite=[];
+let name;
+let link;
+let nameForLocalStorage="favoriteSite";
+let containerForSites=document.getElementById("containerSites");
 
-function save_data(event){
+ containerForSites.onclick=function(event){
+ let target=event.target;
+while(target!=this){
+      if(target.tagName==="BUTTON"){
+         deleteItem(target);
+         return;
+      }
+      
+      target =target.parentNode;
+     }  
+ 
+ };
+
+ function init(){
+    if (localStorage[nameForLocalStorage]) {
+       storedSite=JSON.parse(localStorage[nameForLocalStorage]);
+        for (let i = 0; i < storedSite.length; i++) {
+             name=storedSite[i].name;
+             link=storedSite[i].link;
+            add(name,link);
+    }
+}
+};
+
+function saveData(event){
  event.preventDefault();
  let form =document.forms[0];
- let name=form.elements[0].value;
- let link=form.elements[1].value;
-  if (validate(name,link)){
-  
- let obj= new Site(name,link);
- function Site(name,link){
-
- this.name=name; 
- this.link=link;
- }
- //var obj= {name:name,link:link};
+  name=form.elements[0].value;
+  link=form.elements[1].value;
+ let obj= {name:name,link:link};
+ if (validate(name,link)){
    storedSite.push(obj);
-   localStorage.setItem("storedSite",JSON.stringify(storedSite));
+setItems(nameForLocalStorage,storedSite);
    add(name,link);
  }
 
 document.forms[0].elements[0].value="";
 document.forms[0].elements[1].value="";
 
-}
-function add(name,link){
-    let container = document.getElementsByClassName("container_sites")[0];
-    let section= document.createElement("section");
-    section.className="item";
-    let span=document.createElement("span");
-    span.className="title";
-    span.innerHTML=name;
+};
+
+ function add(name,link){
+    let div=document.createElement("div");
+    div.className="item";
+    let ul=document.createElement("ul");
+    let liFirst=document.createElement("li");
+    let liSecond=document.createElement("li");
+    let liThird=document.createElement("li");
+    ul.className="listOfSites";
+    liFirst.innerHTML=name;
+    ul.appendChild(liFirst);
     let a= document.createElement("a");
     a.className="link";
     a.setAttribute("href",link);
     a.innerHTML="Visit";
+     liSecond.appendChild(a);
+     ul.appendChild(liSecond);
     let button=document.createElement("button");
-    button.className="delete_item";
+    button.className="delete";
     button.innerHTML="I am fed";
-   	section.appendChild(span);
-   	section.appendChild(a);
-   	section.appendChild(button);
-  	container.appendChild(section);
-  }
+     liThird.appendChild(button);
+     ul.appendChild(liThird);
+    div.appendChild(ul);
+    containerForSites.appendChild(div);
 
+  };
 
-
- function init(){
-    if (localStorage.storedSite || JSON.parse(localStorage.getItem("storedSite")).length!=0) {
-       let storedSite=JSON.parse(localStorage.storedSite);
-        for (var i = 0; i < storedSite.length; i++) {
-            let name=storedSite[i].name;
-            let link=storedSite[i].link;
-            add(name,link);
-
-    }
-}
-}
-
-   let container=document.getElementById("container_sites");
-   container.onclick=function(event){
-   var target=event.target;
-     while(target!=this){
-      if(target.tagName==="BUTTON"){
-         delete_item(target);
-         return;
+  function deleteItem(target){
+    let elementForRemove= target.parentNode.parentNode.firstChild.innerHTML;
+	let list=JSON.parse(localStorage.getItem("favoriteSite"));        
+    for (let i = 0; i <list.length; i++) {
+      if (list[i].name===elementForRemove.toLowerCase()) {
+        list.splice(i,1);
       }
-      
-      target =target.parentNode;
-     }   
- 
-}
-
-  function delete_item(target){
-  	let name;
-    let link;
-    let elementToremove= target.parentNode.firstElementChild.innerHTML;
-	let storedSite=JSON.parse(localStorage.getItem("storedSite"));        
-    let newStoredSite= storedSite.filter(function(obj){
-          if (obj.name!=elementToremove) {
-			return obj;
-          }
-             });
-   localStorage.setItem("storedSite",JSON.stringify(newStoredSite));
-   target.parentNode.remove();
+    }
+    target.parentNode.parentNode.parentNode.remove();
+    if (!list.length){
+        localStorage.removeItem("favoriteSite");
+      }
+      else {
+      setItems(nameForLocalStorage,list)
+       
             }
-	
+	};
 
-    function validate(name,link){
+ function setItems(favorite,items){
 
+     localStorage[favorite] = JSON.stringify(items);
+
+ }
+
+  function validate(name,link){
  let reg=/(https?).[/].*[.]/g;
     if (name==="" && link==="") {
-    	alert("Please full texfield");
+    	alert("Please full textfield");
     	return false;
     }
     else if (name==="") {
     	alert("Enter the name");
     	return false;
     }
-
     else if (link==="") {
     	alert("Enter the link");
     	return false;
@@ -110,10 +118,9 @@ function add(name,link){
     else if(!link.match(reg)){
     	alert("Incorrect format");
     return false;
-
-} 
+}
 return true;
-    }
+    };
    
 
     
